5ba8773e332a6539635c5e3219d3c2cb629a8fc2
luiz-bonfioli <lfbonfioli@gmail.com>
2025-04-06 09:00:28 -0300

    Adding Express validators

--- src/molecules/molecule.repository.ts
+++ src/molecules/molecule.repository.ts
@@ -1,12 +1,24 @@
 import {PrismaClient} from "../../generated/prisma";
 import {Molecule} from "./molecule.model";
 
+// Instantiate the Prisma client to interact with the database
 const prisma = new PrismaClient();
 
+/**
+ * Retrieves all molecules from the database.
+ *
+ * @returns An array of Molecule objects.
+ */
 export const findMolecules = async () => {
     return await prisma.molecule.findMany() as Molecule[]
 }
 
+/**
+ * Retrieves a specific molecule by its unique ID.
+ *
+ * @param id - The unique identifier of the molecule.
+ * @returns A Molecule object if found, otherwise `undefined`.
+ */
 export const findMoleculeById = async (id: number) => {
     return await prisma.molecule.findUnique({
         where: {id}
